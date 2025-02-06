be2e22c57bd1a39740d01fc1584d9ebc7e5d9e52
Manuel Marte <mjmarte@bu.edu>
2025-02-06 16:21:38 -0500

    Update precision calculation to match WebGazer's original code

--- main.js
+++ main.js
@@ -237,34 +237,62 @@ function removeAccuracyCircle() {
 
 // Calculate precision from prediction points
 function calculatePrecision(past50Array) {
-    let windowHeight = window.innerHeight;
-    let windowWidth = window.innerWidth;
-    
-    // Retrieve the middle point of the canvas
-    const canvas = document.getElementById("plotting_canvas");
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
 
-    let precision = 0;
-    let dataLength = past50Array[0].length;
-    
-    for (let i = 0; i < dataLength; i++) {
-        // Calculate distance between each prediction and center point
-        let xDiff = past50Array[0][i] - centerX;
-        let yDiff = past50Array[1][i] - centerY;
-        let distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
-        
-        // Calculate precision based on distance
-        // The closer the predictions are to the center point, the higher the precision
-        precision += Math.max(0, 100 - (distance / Math.min(windowWidth, windowHeight) * 100));
+    // Retrieve the last 50 gaze prediction points
+    var x50 = past50Array[0];
+    var y50 = past50Array[1];
+
+    // Calculate the position of the point the user is staring at
+    var staringPointX = windowWidth / 2;
+    var staringPointY = windowHeight / 2;
+
+    var precisionPercentages = new Array(50);
+    calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY);
+    var precision = calculateAverage(precisionPercentages);
+
+    // Return the precision measurement as a rounded percentage
+    return Math.round(precision);
+}
+
+/*
+ * Calculate percentage accuracy for each prediction based on distance of
+ * the prediction point from the centre point (uses the window height as
+ * lower threshold 0%)
+ */
+function calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY) {
+    for (var x = 0; x < 50; x++) {
+        // Calculate distance between each prediction and staring point
+        var xDiff = staringPointX - x50[x];
+        var yDiff = staringPointY - y50[x];
+        var distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+
+        // Calculate precision percentage
+        var halfWindowHeight = windowHeight / 2;
+        var precision = 0;
+        if (distance <= halfWindowHeight && distance > -1) {
+            precision = 100 - (distance / halfWindowHeight * 100);
+        } else if (distance > halfWindowHeight) {
+            precision = 0;
+        } else if (distance > -1) {
+            precision = 100;
+        }
+
+        // Store the precision
+        precisionPercentages[x] = precision;
     }
-    
-    // Calculate average precision
-    precision = precision / dataLength;
-    
-    // Round to 2 decimal places
-    precision = Math.round(precision * 100) / 100;
-    
+}
+
+/*
+ * Calculates the average of all precision percentages calculated
+ */
+function calculateAverage(precisionPercentages) {
+    var precision = 0;
+    for (var x = 0; x < 50; x++) {
+        precision += precisionPercentages[x];
+    }
+    precision = precision / 50;
     return precision;
 }
 
